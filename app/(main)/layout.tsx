0f7ed5687398be0770bbb2c612ca6c8a6fc9d324
Jino <abhkim0803@gmail.com>
2025-06-16 15:28:20 +0700

    update page wrapper and connect db

--- app/(main)/layout.tsx
+++ app/(main)/layout.tsx
@@ -1,16 +1,16 @@
 import Sidebar from "@/components/sidebar";
 import MobileHeader from "@/components/mobile-header";
 
-export default function MainLayout({children}: {children: React.ReactNode}) {
-    return (
-        <>
-          <MobileHeader/>
-          <Sidebar className="hidden lg:flex"/>
-          <main className="pl-[256px] h-full pt-[50px] lg:pt-0">
-            <div className="lg:bg-red-500 h-full">
-              {children}
-            </div>
-          </main>
-        </>
-      );
+export default function MainLayout({ children }: { children: React.ReactNode }) {
+  return (
+    <>
+      <MobileHeader />
+      <Sidebar className="hidden lg:flex" />
+      <main className="pl-[256px] h-full pt-[50px] lg:pt-0">
+        <div className="max-w-[1056px] mx-auto pt-6 h-full">
+          {children}
+        </div>
+      </main>
+    </>
+  );
 }
--- middleware.ts
+++ middleware.ts
@@ -1,6 +1,6 @@
 import { clerkMiddleware, createRouteMatcher } from '@clerk/nextjs/server'
 
-const isPublicRoute = createRouteMatcher(["/"])
+const isPublicRoute = createRouteMatcher(["/", "/learn"])
 
 export default clerkMiddleware(async (auth, req) => {
   if (!isPublicRoute(req)) {
